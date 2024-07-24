647e4d63048b71054a785eb27bc7d029fd65f230
mervezgin <mervezgin@gmail.com>
2024-07-24 09:18:48 +0300

    added startthegame and quitthegame method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR 
+using UnityEditor;
+#endif
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void StartTheGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void QuitTheGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit;
+#endif
     }
 }
